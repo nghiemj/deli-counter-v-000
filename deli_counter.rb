b47318f3302347c7eb5ffeba548d96c9124210d2
James Nghiem <nghiem.j@hotmail.com>
2018-11-16 01:15:04 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,22 @@
-# Write your code here.
+def line(katz_deli) 
+  if katz_deli.empty?
+    puts "The line is currently empty."
+  else
+    string = "The line is currently:"
+    katz_deli.each_with_index do |name, index|
+      string += " #{index +1}. #{name}"
+    end
+    puts string
+  end
+end
+
+
+def take_a_number(deli_line, new_customer)
+  #shove new_customer into deli_line
+  #puts "welcome..." string
+  
+end
+
+def now_serving
+  
+end
