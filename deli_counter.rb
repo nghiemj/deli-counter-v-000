bb60650d327d0f1f6f28cba0fd7226661596668e
James Nghiem <nghiem.j@hotmail.com>
2018-11-16 01:22:01 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,7 +14,7 @@ end
 def take_a_number(deli_line, new_customer)
   deli_line << new_customer
   #shove new_customer into deli_line
-  puts "Welcome, #{new_customer}. You are number 1 in line" string
+  puts "Welcome, #{new_customer}. You are number 1 in line"
 end
 
 def now_serving
