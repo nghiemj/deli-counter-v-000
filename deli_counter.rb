70fd7d0a0ff99bde90b1fdaac996404536e13e64
James Nghiem <nghiem.j@hotmail.com>
2018-11-16 02:38:51 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,7 +16,7 @@ def take_a_number(deli_line, new_customer)
 end
 
 def now_serving(deli_line)
-  if now_serving.empty?
+  if deli_line.empty?
   puts "There is nobody waiting to be served!"
 else 
 end
